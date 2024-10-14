c9d37a550f223988d1feaf774f9125a357e35950
Elbby Skermine <elbbyeasy@gmail.com>
2024-10-14 16:03:28 +0200

    build works

--- db/configFirebase.js
+++ db/configFirebase.js
@@ -24,6 +24,6 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app); 
-const analytics = getAnalytics(app);
 
-export { app, db, auth, storage, analytics };
+export { app, db, auth, storage };
+// manage analytics after 
